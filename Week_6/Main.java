270b1a344cd0592fca922ed60390082cde8cf0d2
vignarohan <103443800+vignarohan@users.noreply.github.com>
2026-08-21 16:56:20 +0800

    Create Main.java

---
+++ Week_6/Main.java
@@ -0,0 +1,15 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        lecturer lecturer1 = new lecturer(
+            "L100",
+            "Dr Ahmad",
+            "Java Programming",
+            "Faculty of Information Technology"
+        );
+
+        lecturer1.displayInfo();
+        lecturer1.displaySubject();
+    }
+}
