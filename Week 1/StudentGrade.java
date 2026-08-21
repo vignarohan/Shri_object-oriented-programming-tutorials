fd5992ec8abba2f0f423500fa06a378a0a0eeb5b
vignarohan <103443800+vignarohan@users.noreply.github.com>
2026-08-21 15:47:03 +0800

    Add StudentGrade.java

---
+++ Week 1/StudentGrade.java
@@ -0,0 +1,15 @@
+public class StudentGrade {
+
+    public static void main(String[] args) {
+
+        String studentName = "Tharcanaa";
+
+        int mark = 80;
+
+        System.out.println("Student Name: " + studentName);
+
+        System.out.println("Mark: " + mark);
+
+    }
+
+}
