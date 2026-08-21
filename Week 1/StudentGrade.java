371f7156b1703665fbf219b45b948c1e1ee42a28
vignarohan <103443800+vignarohan@users.noreply.github.com>
2026-08-21 16:33:45 +0800

    Update StudentGrade.java
    Updated student name and mark variables, and added grade output.

--- Week 1/StudentGrade.java
+++ Week 1/StudentGrade.java
@@ -1,15 +1,23 @@
 public class StudentGrade {
-
     public static void main(String[] args) {
+        String studentName = "Shri";
+        int mark = 85;
+        char grade;
 
-        String studentName = "Tharcanaa";
-
-        int mark = 80;
+        if (mark >= 80) {
+            grade = 'A';
+        } else if (mark >= 70) {
+            grade = 'B';
+        } else if (mark >= 60) {
+            grade = 'C';
+        } else if (mark >= 50) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
 
         System.out.println("Student Name: " + studentName);
-
         System.out.println("Mark: " + mark);
-
+        System.out.println("Grade: " + grade);
     }
-
 }
