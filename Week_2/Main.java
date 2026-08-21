883f465bd18ca109fdf74bbab10118547553c014
vignarohan <103443800+vignarohan@users.noreply.github.com>
2026-08-21 16:37:17 +0800

    Create Main.java

---
+++ Week_2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Shri", 20, 3.75);
+
+        s1.displayInfo();
+        s1.study();
+        s1.takeExam();
+    }
+}
